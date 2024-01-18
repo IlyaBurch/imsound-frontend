f3e2a16a4df0fa67d3f74ae19c212bfefa89dd2f
IlyaBurch <ilyaburch@yandex.ru>
2024-01-18 16:47:44 +0300

    Update getCatalog.ts

--- src/shared/api/getCatalog.ts
+++ src/shared/api/getCatalog.ts
@@ -3,10 +3,10 @@ import axios from "axios";
 // https://imsound.ru/api/catalog - imsound url
 
 export default async function getCatalog() {
-  let data = {}
   try {
     const response = await axios.get("https://dummyjson.com/products/1");
-    data = response;
+    const data = response;
+    return data;
   } catch (error) {
     console.error("Error fetching data:", error);
   }
